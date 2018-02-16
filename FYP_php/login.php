98fafa7bb1b576362b1b9a2e03ccb0ce9443fccf
aloofredchan1999 <30435828+aloofredchan1999@users.noreply.github.com>
2018-02-16 22:43:12 +0800

    Add the code for auto log out, need adjustment

--- FYP_php/login.php
+++ FYP_php/login.php
@@ -10,5 +10,15 @@ if ($username=="admin") {
     exit;
 
 }
+// the code for auto log out, need adjustment
+if(time() - $_SESSION['timestamp'] > 900) { //subtract new timestamp from the old one
+    echo"<script>alert('15 Minutes over!');</script>";
+    unset($_SESSION['username'], $_SESSION['password'], $_SESSION['timestamp']);
+    $_SESSION['logged_in'] = false;
+    header("Location: " . index.php); //redirect to index.php
+    exit;
+} else {
+    $_SESSION['timestamp'] = time(); //set new timestamp
+}
 
  ?>
